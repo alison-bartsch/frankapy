90c133adc35aa92fa06ec5b9dc57e95b3ddc0e1d
tan-may16 <tanmaychinchanikar@gmail.com>
2023-07-08 16:31:03 -0400

    Data collection code

---
+++ scripts/demo_scripts/collect_data.py
@@ -0,0 +1,143 @@
+import cv2
+import numpy as np
+import pyrealsense2 as rs
+from autolab_core import RigidTransform
+from frankapy import FrankaArm
+
+"""
+This file is to use as a reference for how to call each camera by the 
+respective serial number as well as which transform file is applicable
+for each camera.
+"""
+W = 848
+H = 480
+
+# ----- Camera 1 (end-effector) -----
+REALSENSE_INTRINSICS_CAM_1 = "calib/realsense_intrinsics.intr"
+REALSENSE_TF_CAM_1 = "calib/realsense_ee.tf"
+
+
+if __name__ == "__main__":
+    
+    pipeline_1 = rs.pipeline()
+    config_1 = rs.config()
+    config_1.enable_device('220222066259')
+    config_1.enable_stream(rs.stream.depth, W, H, rs.format.z16, 30)
+    config_1.enable_stream(rs.stream.color, W, H, rs.format.bgr8, 30)
+    colorizer = rs.colorizer()
+    p1 = pipeline_1.start(config_1)
+    ds1 = p1.get_device().first_depth_sensor().get_depth_scale()
+    aligned_stream_1 = rs.align(rs.stream.color)
+
+    fa = FrankaArm()
+    
+    # reset franka to its home joints
+    fa.close_gripper()
+    fa.reset_pose()
+    fa.reset_joints()
+    pose = fa.get_pose()
+    print("\nRobot Pose: ", pose)
+    
+    
+    # pose.translation = np.array([3.07052791e-01, -5.60250537e-06, 0.4])
+    # # # pose.rotation = 
+    # fa.goto_pose(pose)
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID') 
+    color_filename = 'data_color.avi' 
+    depth_filename = 'data_depth.avi' 
+    output_fps = 30.0  # Frames per second
+    output_size = (W, H) 
+    color_video = cv2.VideoWriter(color_filename, fourcc, output_fps, output_size)
+    depth_video = cv2.VideoWriter(depth_filename, fourcc, output_fps, output_size)
+
+
+    # for i in range(650):
+    
+    i = 0
+    while(True):
+        # Camera 1
+        frames_1 = pipeline_1.wait_for_frames()
+        frames_1 = aligned_stream_1.process(frames_1)
+        color_frame_1 = frames_1.get_color_frame()
+        color_image_1 = np.asanyarray(color_frame_1.get_data())
+        
+        depth_frame_1 = frames_1.get_depth_frame().as_depth_frame()
+        depth_image_1 = np.asanyarray(depth_frame_1.get_data())
+        depth_colormap = np.asanyarray(colorizer.colorize(depth_frame_1).get_data())
+        depths = np.clip(depth_image_1, 0, 1500)
+        depth_image_1 = np.uint16(depths)*255.0/1500.0
+        
+        
+        
+        # Show the images
+        cv2.imshow("Camera 1", color_image_1)
+        cv2.imshow("Camera 1 Depth", depth_image_1)
+        cv2.imshow("Camera 1 depth colormap", depth_colormap)
+        color_video.write(color_image_1)
+        depth_video.write(depth_image_1)
+        
+        k = cv2.waitKey(1)
+        if k==27:
+            break
+        elif k== ord('s'):
+            print("Saving....")
+            # Save the image
+            cam1_filename_c = "/socket_Imgs/color/" + str(i) + "_cam1.jpg"
+            cam1_filename_d = "/socket_Imgs/depth/" + str(i) + "_cam1.png"
+            
+            # print(np.unique((depth_image_1/255.0)*1500*ds1))
+            # print(np.unique(depth_colormap))
+            
+            cv2.imwrite(cam1_filename_c, color_image_1)
+            cv2.imwrite(cam1_filename_d, depth_image_1)
+            i += 1
+            
+            
+        elif k == ord('x'):
+            T_ee_world = fa.get_pose()
+            print('Current pose: ', T_ee_world)
+            T_ee_world.translation += [0.1, 0., 0.]
+            fa.goto_pose(T_ee_world)
+            print('New pose: ', fa.get_pose())
+            
+        elif k == ord('y'):
+            T_ee_world = fa.get_pose()
+            print('Current pose: ', T_ee_world)
+            T_ee_world.translation += [0., 0.1, 0.]
+            fa.goto_pose(T_ee_world)
+            print('New pose: ', fa.get_pose())
+            
+        elif k == ord('z'):
+            T_ee_world = fa.get_pose()
+            print('Current pose: ', T_ee_world)
+            T_ee_world.translation += [0., 0., 0.1]
+            fa.goto_pose(T_ee_world)
+            print('New pose: ', fa.get_pose())
+            
+            
+        elif k == ord('c'):
+            T_ee_world = fa.get_pose()
+            print('Current pose: ', T_ee_world)
+            T_ee_world.translation -= [0.1, 0., 0.]
+            fa.goto_pose(T_ee_world)
+            print('New pose: ', fa.get_pose())
+            
+        elif k == ord('u'):
+            T_ee_world = fa.get_pose()
+            print('Current pose: ', T_ee_world)
+            T_ee_world.translation -= [0., 0.1, 0.]
+            fa.goto_pose(T_ee_world)
+            print('New pose: ', fa.get_pose())
+            
+        elif k == ord('a'):
+            T_ee_world = fa.get_pose()
+            print('Current pose: ', T_ee_world)
+            T_ee_world.translation -= [0., 0., 0.1]
+            fa.goto_pose(T_ee_world)
+            print('New pose: ', fa.get_pose())
+            
+        
+            
+            
+    cv2.destroyAllWindows()
