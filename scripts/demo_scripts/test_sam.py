b29a58b9c0d0c12d9d1974617e93828cc904e3a1
Tanmay Chinchanikar <tanmaychinchanikar@gmail.com>
2023-08-03 11:15:05 -0400

    YOLO + SAM detector

--- scripts/demo_scripts/test_sam.py
+++ scripts/demo_scripts/test_sam.py
@@ -8,33 +8,13 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
-
+import time
 W = 848
 H = 480
 
 # ----- Camera 1 (end-effector) -----
 REALSENSE_INTRINSICS_CAM_1 = "calib/realsense_intrinsics.intr"
 REALSENSE_TF_CAM_1 = "calib/realsense_ee.tf"
-
-def show_mask(mask, ax, random_color=False):
-    if random_color:
-        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
-    else:
-        color = np.array([30/255, 144/255, 255/255, 0.6])
-    h, w = mask.shape[-2:]
-    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
-    ax.imshow(mask_image)
-
-def show_points(coords, labels, ax, marker_size=375):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-
-def show_box(box, ax):
-    x0, y0 = box[0], box[1]
-    w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
 
 if __name__ == '__main__':
     
@@ -57,7 +37,7 @@ if __name__ == '__main__':
     else:
         warnings.warn('CUDA not found! Execution may be slow......')
 
-	
+
 	#============= Loading the SAM Model =======================
 #    url = 'https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth'
 #    filename = 'sam_vit_h_4b8939.pth'
@@ -66,77 +46,58 @@ if __name__ == '__main__':
     model_path_SAM = os.path.join('Models', 'sam_vit_h_4b8939.pth')
     print(model_path_SAM)
 
-    if os.path.isfile(model_path_SAM):
-        print("SAM model file exists!")
-        model_type = "default"
 
-        sam = sam_model_registry[model_type](checkpoint=model_path_SAM)
-        sam.to(device=device)
+    sam_checkpoint = "sam_vit_h_4b8939.pth"
+    model_type = "vit_h"
 
+    # device = "cuda"
 
-        #TODO: Change SAM parameters
-        SAM = SamAutomaticMaskGenerator(sam)
-        # SAM = SamAutomaticMaskGenerator(model=sam,
-        #                                 # points_per_side=32,
-        #                                 # pred_iou_thresh=0.86,
-        #                                 # stability_score_thresh=0.92,
-        #                                 # crop_n_layers=1,
-        #                                 crop_n_points_downscale_factor=2,
-        #                                 min_mask_region_area=100,  # Requires open-cv to run post-processing
-        #                                 ) 
-                            
-    else:
-        warnings.warn("The file does not exits.")
-        
-    #    if os.path.isfile("sam_vit_h_4b8939.pth"):
-    #        print("File already exists!")
-    #
-    #    else:
-    #        urllib.request.urlretrieve(url, filename, reporthook=show_progress)
-    #        print("\nDownload complete!")
-        
-    #    sam_checkpoint = "sam_vit_h_4b8939.pth"
+    sam = sam_model_registry[model_type](checkpoint=model_path_SAM)
+    sam.to(device=device)
 
-    # Load the video
-    video_name = 'videos/data_color.avi'
-    video_path = os.path.join('', video_name) # Load the appropriate video path 
-    if os.path.isfile(video_path):
-        print("Video file exists!")
-        video = cv2.VideoCapture(video_path)
-    else:
-        warnings.warn("The file does not exits.")
+    predictor = SamPredictor(sam)
 
 
-    # Specifying output video file:
-    # output_path = os.path.join('Videos/Test Videos', 'sam_mask_live_centroid_video.mp4')  # Load the appropriate video path
-    # fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    # output_video = cv2.VideoWriter(output_path, fourcc, 30.0, (int(video.get(3)), int(video.get(4))))
+    video_path = 'data_color.avi'
+    cap = cv2.VideoCapture(video_path)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-    # Get total number of frames
-    num_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+    output_path = 'output_video.avi'
+    output_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'XVID'), fps, output_size)
 
-    frame_counter = 0
-    while video.isOpened():
-        ret, frame = video.read()
-        
-        # Break the loop if the video ends
+    # Process each frame of the video
+    while cap.isOpened():
+        start = time.time()
+        ret, frame = cap.read()
         if not ret:
             break
-        cropped_mask = SAM.generate(frame)
-        for i, (mask) in enumerate(zip(mask)):
-            plt.figure(figsize=(10,10))
-            plt.imshow(frame)
-            show_mask(mask, plt.gca())
-            # show_points(input_point, input_label, plt.gca())
-            plt.axis('off')
-            plt.show()
-        #frame_counter += 1
 
-    print("Process Finished!!!")
-    # print(f"Output video saved at: {output_path}")
-    video.release()
-    # output_video.release()
+        # Convert frame from BGR to RGB
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        input_point = np.array([[480,240]])
+        input_label = np.array([1])
+        predictor.set_image(frame)
+        masks, scores, logits = predictor.predict(
+        point_coords=input_point,
+        point_labels=input_label,
+        multimask_output=False,
+          )
+        h, w = masks.shape[-2:]
+        mask_image = masks.reshape(h, w, 1)
+        final_sam_mask = np.concatenate([mask_image] * 3, axis=2)
+        print(np.unique(mask_image))
+        end = time.time()
+        print(end - start)
+        cv2.imshow('Frame', mask_image)
+        cv2.imshow('Mask', final_sam_mask)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release video capture and writer objects
+    cap.release()
+    out.release()
+
+    # Close all OpenCV windows
     cv2.destroyAllWindows()
-
-
-
---
+++ scripts/demo_scripts/test_yolo_sam.py
@@ -0,0 +1,158 @@
+import cv2
+import torch
+from pathlib import Path
+from ultralytics import YOLO
+import numpy as np
+from segment_anything import sam_model_registry, SamPredictor
+import time
+import os
+import open3d as o3d
+import warnings 
+
+device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+if device.type == 'cuda':
+        print('CUDA is found! Executing on %s.......'%torch.cuda.get_device_name(0))
+else:
+    warnings.warn('CUDA not found! Execution may be slow......')
+
+# yolo_weights = 'segment_best.pt'
+model_path_YOLO = os.path.join('Models', 'best.pt')
+if os.path.isfile(model_path_YOLO):
+        print("YOLO model file exists!")
+else: 
+    warnings.warn("YOLO model does not exist.")
+    quit()
+
+SAM = True
+OPEN3D = False
+
+model = YOLO(model_path_YOLO) 
+model.to(device)
+
+	#============= Loading the SAM Model =======================
+#    url = 'https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth'
+#    filename = 'sam_vit_h_4b8939.pth'
+
+model_type = "vit_h"
+
+model_path_SAM = os.path.join('Models', 'sam_vit_h_4b8939.pth')
+
+if os.path.isfile(model_path_SAM):
+        print("SAM model file exists!")
+else: 
+    warnings.warn("SAM model does not exist.")
+    quit()
+
+if SAM:
+    sam = sam_model_registry[model_type](checkpoint=model_path_SAM)
+    sam.to(device=device)
+    predictor = SamPredictor(sam)
+
+
+color_path = 'data_color.avi'
+depth_path = 'data_depth.avi'
+cap = cv2.VideoCapture(color_path)
+dep = cv2.VideoCapture(depth_path)
+
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+fps_depth = int(dep.get(cv2.CAP_PROP_FPS))
+
+
+output_path = 'output_video.avi'
+output_size = (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'XVID'), fps, output_size)
+
+
+while cap.isOpened():
+    ret, color = cap.read()
+    ret_d, depth = dep.read()
+    
+    if (not ret) or (not ret_d) :
+        break
+
+    color = cv2.cvtColor(color, cv2.COLOR_BGR2RGB)
+
+
+    if SAM:
+        predictor.set_image(color)
+        final_sam_mask = None
+    
+    yolo_results = model(color)
+    yolo_boxes = yolo_results[0].boxes
+    yolo_masks = yolo_results[0].masks
+    
+    
+    for i in range(yolo_boxes.shape[0]):
+        box = yolo_boxes[i].xyxy.tolist()[0]
+        x1, y1, x2, y2 = int(box[0]), int(box[1]), int(box[2]), int(box[3])
+        
+        if SAM:
+            x_c = int((x1 + x2) / 2)
+            y_c = int((y1 + y2)/2)
+            input_point = np.array([[x_c,y_c]])
+            input_label = np.array([1])
+            
+            sam_masks, scores, logits = predictor.predict(
+            point_coords=input_point,
+            point_labels=input_label,
+            multimask_output=False,)
+            
+            h, w = sam_masks.shape[-2:]
+            mask_image = sam_masks.reshape(h, w, 1)
+            
+            if final_sam_mask is None:
+                final_sam_mask = mask_image
+            else:
+                final_sam_mask += mask_image
+            
+        color = cv2.rectangle(color, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+    if SAM:
+        final_sam_mask = np.concatenate([final_sam_mask] * 3, axis=2)
+
+        
+
+        
+        cv2.imshow("YOLO + SAM mask", final_sam_mask.astype(np.float32))
+    if OPEN3D:
+        depths = ((depth*final_sam_mask)*1500/255.0).astype(np.uint8)
+        color_image = o3d.geometry.Image(color)
+        depth_image = o3d.geometry.Image(depths)
+        
+        ds1 = 0.001
+        cam = o3d.camera.PinholeCameraIntrinsic(w, h, 621.70715, 621.9764, 323.3644, 244.8789)
+        rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(
+                    color_image,
+                    depth_image,
+                    depth_scale=1.0 / ds1,
+                    depth_trunc= 1,
+                    convert_rgb_to_intensity=False)
+        temp = o3d.geometry.PointCloud.create_from_rgbd_image(
+                    rgbd_image, cam)
+        cl, ind = temp.remove_statistical_outlier(nb_neighbors=200,
+                                                        std_ratio=1.0)
+        o3d.visualization.draw_geometries([temp.select_by_index(ind)])
+        cv2.imshow("Depths", depths.astype(np.float32))
+
+    color = cv2.cvtColor(color, cv2.COLOR_RGB2BGR)
+
+    # Write the frame to the output video
+    out.write(color)
+
+    # Display the frame (optional)
+    cv2.imshow('Frame', color)
+
+    if yolo_masks is not None:
+        mask = np.sum(yolo_masks.data.cpu().numpy(), axis= 0)
+        cv2.imshow('YOLO Segmentation Mask', mask)
+    else: continue
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release video capture and writer objects
+cap.release()
+out.release()
+
+# Close all OpenCV windows
+cv2.destroyAllWindows()
